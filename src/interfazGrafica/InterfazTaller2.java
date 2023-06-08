a006f3618a750d451c7f75f8f5c94de0f5f9a50d
Federico <Federico@DESKTOP-CN4EU2E>
2023-06-08 12:13:09 -0300

    Carga inicial

---
+++ src/interfazGrafica/InterfazTaller2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package interfazGrafica;
+
+/**
+ *
+ * @author Federico
+ */
+public class InterfazTaller2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login sesion = new Login();
+        sesion.setVisible(true);
+        sesion.setLocationRelativeTo(null);
+        
+   
+        
+    }
+    
+}
