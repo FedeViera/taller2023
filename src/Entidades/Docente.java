835250210aa954953e7ffdbcbb2f36d05f2ee645
Federico Viera <110137888+FedeViera@users.noreply.github.com>
2023-08-22 17:00:31 -0300

    n
    n

--- src/Entidades/Docente.java
+++ src/Entidades/Docente.java
@@ -3,14 +3,12 @@ package Entidades;
 public class Docente extends Usuario
 {
     private int grado;
-    private String especialidad;
-    private String gradoAcademico;
+    private String asignatura;
 
-    public Docente(int grado, String especialidad, String gradoAcademico, int cedula, String nombre, String apellido, String usuario, String contrasenia, String cargo) {
+    public Docente(int grado, String asignatura, int cedula, String nombre, String apellido, String usuario, String contrasenia, String cargo) {
         super(cedula, nombre, apellido, usuario, contrasenia, cargo);
         this.grado = grado;
-        this.especialidad = especialidad;
-        this.gradoAcademico = gradoAcademico;
+        this.asignatura = asignatura;
     }
     
     
